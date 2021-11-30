f6789f2c90c802bddcb3d24c1b33e681a68b6f4e
jlee <lee0916jh@gmail.com>
2021-11-30 02:05:17 -0600

    submission finalized

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,10 @@
-export const host = 'http://3.131.85.159:5000';
+export const host = 'http://localhost:5000';
 
 export const catalystOptions = ['Copper', 'Platinum', 'Nickel', 'Palladium', 'Palladium Thin F'].sort()
 export const prepNameOptions = ['Annealing', 'Growing', 'Cooling']
-export const carbonSourceOptions = ['CH4']
-export const shapeOptions = []
+export const carbonSourceOptions = ['CH4'].sort()
+export const shapeOptions = ['Example Shape']
+export const materialNameOptions = ['Graphene']
 export const firstnames = []
 export const lastnames = []
 export const institutions = []
